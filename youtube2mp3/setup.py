8bd6454883872b942fefac35ee6437960385f733
R3i-san <58115082+R3i-san@users.noreply.github.com>
2022-11-16 17:36:04 +0100

    Update setup.py

--- youtube2mp3/setup.py
+++ youtube2mp3/setup.py
@@ -1,2 +1,14 @@
 import os
-os.system("py -m pip install youtube_dl")
+
+choice = input('''---------------------------------------------\n
+These packages will be installed : \n
+youtube_dl \n
+yt_dlp n
+moviepy \n
+---------------------------------------------\n
+>>>Enter to continue''')
+
+if(choice == ''):
+	os.system("py -m pip install youtube_dl")
+	os.system("py -m pip install yt_dlp")
+	os.system("py -m pip install moviepy")
